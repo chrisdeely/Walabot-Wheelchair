0773009291053ea7d109625b6de1e33b4f94445d
Chris Deely <chris@webdeely.com>
2017-04-15 20:54:27 +0000

    sound an alert on the web client when a target approaches

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Walabot-Wheelchair
 An application leveraging the Walabot sensor array for collision prevention for wheelchair users
+
+Alarm MP3 credit: https://www.freesound.org/people/coltonmanz/sounds/381382/
--- webapp/node/app.js
+++ webapp/node/app.js
@@ -1,9 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var path = require('path');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket){
